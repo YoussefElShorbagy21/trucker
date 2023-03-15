52e92c9ab057596858278687845ffc07f827251f
EL Shorbagy <youssefelshorbagy21@gmail.com>
2023-03-15 16:32:58 +0200

    change all model because error

---
+++ lib/modules/customer/screens/chose_app/chose_app.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:login/modules/customer/screens/Login%20Screen/loginScreen.dart';
+import 'package:login/shared/resources/app_localizations.dart';
+
+import '../../../../shared/resources/color_manager.dart';
+import '../../../../shared/resources/font_manager.dart';
+
+
+
+class ChoseApp extends StatelessWidget {
+  const ChoseApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Hello_app'.tr(context),
+             textAlign: TextAlign.center,
+             style: const TextStyle(
+               fontWeight: FontWeight.bold,
+               fontSize: 20,
+             ),),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 2,
+            ),
+            Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(
+            horizontal: MediaQuery.of(context).size.width/35,
+          ),
+          child: TextButton(
+            onPressed:(){
+              Navigator.of(context).push(MaterialPageRoute(builder: (_) => LoginScreen()));
+            } ,
+            style: TextButton.styleFrom(
+              shape: const StadiumBorder(),
+              backgroundColor: ColorManager.black,
+              padding: const EdgeInsets.symmetric(vertical: 14),
+            ),
+            child:  Text("Customer".tr(context),
+              style: TextStyle(
+                color: ColorManager.white,
+                fontFamily: FontConstants.fontFamily,
+                fontSize: FontSize.s22,
+                fontWeight: FontWeightManager.semiBold,
+              ),
+            ),
+          ),
+        ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 20,
+            ),
+            Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width/35,
+        ),
+        child: TextButton(
+          onPressed:(){
+            Navigator.of(context).push(MaterialPageRoute(builder: (_) => LoginScreen()));
+          } ,
+          style: TextButton.styleFrom(
+            shape: const StadiumBorder(),
+            backgroundColor: ColorManager.black,
+            padding: const EdgeInsets.symmetric(vertical: 14),
+          ),
+          child:  Text("service_provider".tr(context),
+            style: TextStyle(
+              color: ColorManager.white,
+              fontFamily: FontConstants.fontFamily,
+              fontSize: FontSize.s22,
+              fontWeight: FontWeightManager.semiBold,
+            ),
+          ),
+        ),
+    ),
+          ],
+        ),
+      ),
+    );
+  }
+}
